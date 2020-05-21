81c3d307b4d17788eb2c92b9f3945a112f1a3e31
Youngmin Chung <y_chung2@fanshaweonline.ca>
2020-05-21 00:42:06 -0400

    First commit

---
+++ src/build/precache-manifest.75bc8ceb482626ce69867f8573878757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc38baf31e2e91a6eab82f2c7ae57ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8ef37a1cb84e451e61",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "6b24c0248e4cc554c66b",
+    "url": "/static/js/2.99650adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99650adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8ef37a1cb84e451e61",
+    "url": "/static/js/main.79273599.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
